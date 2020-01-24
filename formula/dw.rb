04ebe8ea7cef7f3ae23063ec629d829c6d362ccb
afelisatti <afelisatti@mulesoft.com>
2020-01-24 18:36:34 -0300

    Revert "Update to latest version"
    This reverts commit ab3850bd680714703e0a4f7fe2814f22166be55c.

--- formula/dw.rb
+++ formula/dw.rb
@@ -1,9 +1,9 @@
 class Dw < Formula
   desc "DataWeave CLI"
   homepage "https://github.com/mulesoft-labs/data-weave-native"
-  url "https://github.com/mulesoft-labs/data-weave-native/releases/download/v2.3.0-20200124/native-cli-2.3.0-20200124-native-distro-osx.zip"
-  sha256 "d349da27a05c383a0ec4f3de9f1722c5cb799c4ec1ccb141862e166cebb88802"
-  version "2.3.0-20200124"
+  url "https://github.com/mulesoft-labs/data-weave-native/releases/download/v2.3.0-20191128/native-cli-2.3.0-20191128-native-distro-osx.zip"
+  sha256 "6f1a76e43d1f5b7312bd17ab86b713a0dba3f095b031a4f89da30df283cbd4ac"
+  version "2.3.0-20191128"
 
   def install
     prefix.install "bin"
