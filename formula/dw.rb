f5ccbdfa13269770dccc776b0356888b9e5dab8c
Mariano de Achaval <mariano.achaval@mulesoft.com>
2020-02-12 10:59:32 -0300

    Use latest version

--- formula/dw.rb
+++ formula/dw.rb
@@ -1,9 +1,9 @@
 class Dw < Formula
   desc "DataWeave CLI"
   homepage "https://github.com/mulesoft-labs/data-weave-native"
-  url "https://github.com/mulesoft-labs/data-weave-native/releases/download/v2.3.0-20191128/native-cli-2.3.0-20191128-native-distro-osx.zip"
-  sha256 "6f1a76e43d1f5b7312bd17ab86b713a0dba3f095b031a4f89da30df283cbd4ac"
-  version "2.3.0-20191128"
+  url "https://github.com/mulesoft-labs/data-weave-native/releases/download/v2.3.0-20200212/native-cli-2.3.0-20200212-native-distro-osx.zip"
+  sha256 "5067c42179f781bd14025e9d8fe662179c929b2d3fd0d71677f415a9c9bfc30b"
+  version "2.3.0-20200212"
 
   def install
     prefix.install "bin"
