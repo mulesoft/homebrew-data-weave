df971dfad9fac7905f4162de9f0a366fb5b40e06
Leandro Shokida <leandroshokida@gmail.com>
2020-06-24 20:22:17 -0300

    Updating to use dw 2.3.1

--- formula/dw.rb
+++ formula/dw.rb
@@ -1,9 +1,9 @@
 class Dw < Formula
   desc "DataWeave CLI"
   homepage "https://github.com/mulesoft-labs/data-weave-native"
-  url "https://github.com/mulesoft-labs/data-weave-native/releases/download/v2.3.0-20200212/native-cli-2.3.0-20200212-native-distro-osx.zip"
-  sha256 "5067c42179f781bd14025e9d8fe662179c929b2d3fd0d71677f415a9c9bfc30b"
-  version "2.3.0-20200212"
+  url "https://github.com/mulesoft-labs/data-weave-native/releases/download/v2.3.1-20200624/native-cli-2.3.1-20200624-native-distro-osx.zip"
+  sha256 "0130bdccc4549ddb32a7d549a27549eac8361504866cbe82a4d87323aed05358"
+  version "2.3.1-20200624"
 
   def install
     prefix.install "bin"
