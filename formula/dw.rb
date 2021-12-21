1b6b5ef8a6b87b2dedc066e2d24663de415b5ac6
Mariano De Achaval <mdeachaval@salesforce.com>
2021-12-21 17:30:14 -0300

    Release new version of the cli

--- formula/dw.rb
+++ formula/dw.rb
@@ -1,9 +1,9 @@
 class Dw < Formula
   desc "DataWeave CLI"
   homepage "https://github.com/mulesoft-labs/data-weave-cli"
-  url "https://github.com/mulesoft-labs/data-weave-cli/releases/download/v1.0.15/dw-1.0.15-macOS"
-  sha256 "c9e179c570a11c67b580b4d446123d48c4cc3b8f05f67f0c7d89e7cbd18ab2bd"
-  version "2.5.0-20212511"
+  url "https://github.com/mulesoft-labs/data-weave-cli/releases/download/v1.0.16/dw-1.0.16-macOS"
+  sha256 "154615e4f0483a5f7cf4d35c570c2f7eab6f583e9a2d96a6bb0cb8c15b45e6a7"
+  version "2.5.0-20212112"
 
   def install
     prefix.install "bin"
