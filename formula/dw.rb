84edeabe7e4168ba0b0e6b2875eb892239b5ac7c
Mariano de Achaval <mariano.achaval@mulesoft.com>
2023-03-30 12:13:49 -0300

    Update dw.rb

--- formula/dw.rb
+++ formula/dw.rb
@@ -1,9 +1,9 @@
 class Dw < Formula
   desc "DataWeave CLI"
   homepage "https://github.com/mulesoft-labs/data-weave-cli"
-  url "https://github.com/mulesoft-labs/data-weave-cli/releases/download/v1.0.32/dw-1.0.32-macOS"
-  sha256 "1ce02b415b4a4c4593947ec8c1c75763863466449da66388aba4f2e7d87a07e7"
-  version "2.6.0-20230217"
+  url "https://github.com/mulesoft-labs/data-weave-cli/releases/download/v1.0.33/dw-1.0.33-macOS"
+  sha256 "4a93a029ec20a2fddc7ebe3193df789acaa06037b90a4448fb96c2190af6c630"
+  version "2.6.0-20230330"
 
   def install
     prefix.install "bin"
