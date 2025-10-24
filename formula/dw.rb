a40860cdde2f3e7dff50732fa7ed0cf3d87af1bc
Mariano de Achaval <mariano.achaval@mulesoft.com>
2025-10-24 19:44:10 -0300

    Update sha256 checksum for dw.rb

--- formula/dw.rb
+++ formula/dw.rb
@@ -2,7 +2,7 @@ class Dw < Formula
   desc "DataWeave CLI"
   homepage "https://github.com/mulesoft-labs/data-weave-cli"
   url "https://github.com/mulesoft-labs/data-weave-cli/releases/download/v1.0.36/dw-1.0.36-macOS"
-  sha256 "d503f000c24bf0a7701df917561b930bccfc98a922b6425065e13c93f73831fe"
+  sha256 "21d116d8c3d13bc815fd755bde3d64e095c3348d73a6709417647066a89a810a"
   version "2.11.0-20251024"
 
   def install
